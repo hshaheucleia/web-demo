0e213d7699dfe5329f6cb01500a8f0ce8a1368fb
Arjun Atul Kallawar <arjun.kallawar@eucleiacorp.com>
2013-03-25 23:42:46 +0530

    Resolution for following tickets - 27, 26, 24, 19, 18, 17

--- wsgi.py
+++ wsgi.py
@@ -6,3 +6,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'demo')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'demo.settings_dotcloud'
 application = django.core.handlers.wsgi.WSGIHandler()
+
+path = '/home/ubuntu/eclweb/demo'
+if path not in sys.path:
+    sys.path.append(path)
