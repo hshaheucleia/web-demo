6e95e88eb3a16088d30e93d552664a7bff0d812b
Arjun Atul Kallawar <arjun.kallawar@eucleiacorp.com>
2013-03-26 17:00:43 +0530

    Demo data added. Creating JSON to save preferences

--- demo/settings.py
+++ demo/settings.py
@@ -18,8 +18,8 @@ DATABASES = {
         'NAME': 'eucleceu_educareer_stg',                      # Or path to database file if using sqlite3.
         'USER': 'eucleceu_edu',                      # Not used with sqlite3.
         'PASSWORD': 'eucleceu_edu',                  # Not used with sqlite3.
-        'HOST': '199.79.63.83',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
