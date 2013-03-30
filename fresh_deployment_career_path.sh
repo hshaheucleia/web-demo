37064d36772f22ce683dcc22165684ffddb04a6f
Arjun Atul Kallawar <arjun.kallawar@eucleiacorp.com>
2013-03-30 13:09:12 +0530

    Update to fresh deployment script

--- fresh_deployment_career_path.sh
+++ fresh_deployment_career_path.sh
@@ -3,7 +3,7 @@ git pull
 wait
 cp -rf /home/ubuntu/eclweb/settings.py /home/ubuntu/eclweb/demo/demo/
 wait
-mysql ecl_careerpath_tst  -u'edu_user' -p'test_environment_user' < create_databse.sql
+mysql ecl_careerpath_tst  -u'edu_user' -p'test_environment_user' < create_database.sql
 wait
 python manage.py syncdb
 wait
